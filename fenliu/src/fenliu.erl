71d9f5c8cec2841aba7f7e939480aff173a0b9a6
unknown <yonghuan@3CNL09495.NOE.Nokia.com>
2012-05-18 18:09:06 +0800

    eunit all ok about to change fenliu design by removing maxfails and timeout

--- fenliu/src/fenliu.erl
+++ fenliu/src/fenliu.erl
@@ -1,6 +1,52 @@
 -module(fenliu).
 
--export([resolve_server/1, server_name/1]).
+-export([
+	 add_server/1, add_server/2, add_server/3, add_server/4, remove_server/1,
+	 get_nodes/1, add_nodes/2, remove_nodes/2,
+	 resolve_server/1, server_name/1
+	]).
+
+-ifdef(TEST).
+-include_lib("eunit/include/eunit.hrl").
+-behaviour(gen_server).
+-export([init/1, handle_call/3, handle_cast/2, handle_info/2, terminate/2, code_change/3]).
+init([]) -> {ok, {}}.
+handle_call(stop, _From, State) -> {stop, normal, ok, State};
+handle_call({wait, Time}, _From, State) -> timer:sleep(Time), {reply, {ok, Time}, State};
+handle_call(Request, _From, State) -> {reply, {ok, Request}, State}.
+handle_cast(_Msg, State) -> {noreply, State}.
+handle_info(_Info, State) -> {noreply, State}.
+terminate(_Reason, _State) -> ok.
+code_change(_OldVsn, State, _Extra) -> {ok, State}.
+-endif.
+
+
+add_server(ServerName) -> add_server(ServerName, [node()]).
+add_server(ServerName, Nodes) when is_list(Nodes) -> add_server(ServerName, Nodes, 5, 5000);
+add_server(ServerName, MaxFails) when is_integer(MaxFails) -> add_server(ServerName, [node()], MaxFails, 5000).
+add_server(ServerName, Nodes, MaxFails) when is_list(Nodes), is_integer(MaxFails) -> add_server(ServerName, Nodes, MaxFails, 5000);
+add_server(ServerName, MaxFails, CallTimeout) when is_integer(MaxFails), is_integer(CallTimeout) -> add_server(ServerName, [node()], MaxFails, CallTimeout).
+add_server(ServerName, Nodes, MaxFails, CallTimeout) when is_atom(ServerName), is_list(Nodes), is_integer(MaxFails), is_integer(CallTimeout) ->
+    ChildSpec = {server_name(ServerName), 
+		 {fenliu_server, start_link, [ServerName, Nodes, MaxFails, CallTimeout]},
+		 permanent,
+		 5000,
+		 worker,
+		 [fenliu_server]},
+    supervisor:start_child(fenliu_sup, ChildSpec).
+
+remove_server(ServerName) ->
+    supervisor:terminate_child(fenliu_sup, server_name(ServerName)),
+    supervisor:delete_child(fenliu_sup, server_name(ServerName)).
+
+get_nodes(ServerName) ->
+    gen_server:call(server_name(ServerName), nodes).
+
+add_nodes(ServerName, Nodes) when is_list(Nodes) ->
+    gen_server:cast(server_name(ServerName), {add_nodes, Nodes}).
+
+remove_nodes(ServerName, Nodes) when is_list(Nodes) ->
+    gen_server:cast(server_name(ServerName), {remove_nodes, Nodes}).
 
 resolve_server(ServerName) ->
     case whereis(server_name(ServerName)) of
@@ -13,3 +59,157 @@ resolve_server(ServerName) ->
 server_name(ServerName) ->
     list_to_atom("fenliu_" ++ atom_to_list(ServerName)).
  
+%%%===================================================================
+%%% Unit tests
+%%%===================================================================
+
+-ifdef(TEST).
+
+-define(T(X), {??X, fun X/0}).
+
+all_test_() ->
+    {inorder,
+     [
+      ?T(setup0),
+
+      ?T(test0),
+      ?T(test1),
+      ?T(test2),
+      ?T(test3),
+      ?T(test4),
+      ?T(test5),
+      ?T(test6),
+      ?T(test7),
+%      ?T(test8),
+
+      ?T(cleanup0)
+     ]
+    }.
+
+setup0() ->
+    ok = application:start(fenliu),
+    gen_server = resolve_server(simple_server),
+    {ok, _} = gen_server:start({local, simple_server}, ?MODULE, [], []),
+    {ok, _} = gen_server:start({local, server2}, ?MODULE, [], []),
+    {ok, _} = gen_server:start({local, server3}, ?MODULE, [], []),
+
+    ok.
+
+cleanup0() ->
+    ok = gen_server:call(simple_server, stop),
+    ok = gen_server:call(server2, stop),
+    ok = gen_server:call(server3, stop),
+    ok = application:stop(fenliu).
+    
+test0() ->
+    {ok, 0} = gen_server:call(simple_server, 0),
+    {ok, Pid} = add_server(simple_server),
+    Pid = whereis(server_name(simple_server)),
+    fenliu_simple_server = server_name(simple_server),
+    fenliu_server = resolve_server(simple_server),
+    {ok, 1} = fenliu_server:call(simple_server, 1),
+    ok.
+
+test1() ->
+    ok = remove_server(simple_server),
+    {ok, 0} = gen_server:call(simple_server, 0),
+    {'EXIT', {noproc, _}} = (catch fenliu_server:call(simple_server, test)),
+    ok = fenliu_server:cast(simple_server, test),
+    ok.
+
+test2() ->
+    Node = node(),
+
+    {ok, _} = add_server(simple_server, 5),
+    {ok, 0} = gen_server:call(simple_server, 0),
+    {ok, 1} = fenliu_server:call(simple_server, 1),
+    {ok, [{Node, 5}]} = get_nodes(simple_server),
+    ok.
+
+test3() ->
+    Node = node(),
+
+    ok = add_nodes(simple_server, [a@node, b@node, c@node]),
+    {ok, [{Node, 5}, {a@node, 5}, {b@node, 5}, {c@node, 5}]} = get_nodes(simple_server),
+
+    ok = remove_nodes(simple_server, [b@node]),
+    {ok, [{Node, 5}, {a@node, 5}, {c@node, 5}]} = get_nodes(simple_server),
+
+    ok = remove_nodes(simple_server, [a@node]),
+    {ok, [{Node, 5}, {c@node, 5}]} = get_nodes(simple_server),
+
+    ok = remove_nodes(simple_server, [c@node]),
+    {ok, [{Node, 5}]} = get_nodes(simple_server),
+
+    ok = remove_nodes(simple_server, [Node]),
+    {ok, []} = get_nodes(simple_server),
+
+    {error, run_out_of_nodes} = fenliu_server:call(simple_server, 1),
+    ok = fenliu_server:cast(simple_server, 2),
+    ok.
+
+test4() ->
+    Node = node(),
+
+    ok = add_nodes(simple_server, [Node, bad@node]),
+    {ok, [{Node, 5}, {bad@node, 5}]} = get_nodes(simple_server),
+
+    ok = fenliu_server:cast(simple_server, 0),
+    {ok, [{bad@node, 5}, {Node, 5}]} = get_nodes(simple_server),
+
+    ok = fenliu_server:cast(simple_server, 0),
+    {ok, [{Node, 5}, {bad@node, 5}]} = get_nodes(simple_server),
+
+    ok = fenliu_server:cast(simple_server, 0),
+    {ok, [{bad@node, 5}, {Node, 5}]} = get_nodes(simple_server),
+    
+    ok.
+
+test5() ->
+    Node = node(),
+
+    {ok, 1} = fenliu_server:call(simple_server, 1),
+    {ok, [{bad@node, 4}, {Node, 5}]} = get_nodes(simple_server),
+
+    {ok, 2} = fenliu_server:call(simple_server, 2),
+    {ok, [{bad@node, 3}, {Node, 5}]} = get_nodes(simple_server),
+
+    {ok, 3} = fenliu_server:call(simple_server, 3),
+    {ok, [{bad@node, 2}, {Node, 5}]} = get_nodes(simple_server),
+
+    {ok, 4} = fenliu_server:call(simple_server, 4),
+    {ok, [{bad@node, 1}, {Node, 5}]} = get_nodes(simple_server),
+
+    {ok, 6} = fenliu_server:call(simple_server, 6),
+    {ok, [{bad@node, 0}, {Node, 5}]} = get_nodes(simple_server),
+
+    {ok, 7} = fenliu_server:call(simple_server, 7),
+    {ok, [{Node, 5}]} = get_nodes(simple_server),
+   
+    ok.
+
+test6() ->
+    Node = node(),
+
+    {ok, _} = add_server(server2, [bad2@node, Node], 1),
+    
+    {ok, [{bad2@node, 1}, {Node, 1}]} = get_nodes(server2),
+
+    {ok, 1} = fenliu_server:call(server2, 1),
+    {ok, [{bad2@node, 0}, {Node, 1}]} = get_nodes(server2),
+
+    ok = fenliu_server:cast(server2, 3),
+    {ok, [{Node, 1}]} = get_nodes(server2),
+
+    ok.
+
+test7() ->    
+    {ok, _} = add_server(server3, 1, 10),
+
+    io:format(user, "node=~p~n", [get_nodes(server3)]),
+    {ok, 1} = fenliu_server:call(server3, 1),
+    {'EXIT', {timeout, _}} = (catch fenliu_server:call(server3, {wait, 100})),
+    
+    ok.
+    
+-endif.
--- fenliu/src/fenliu_server.erl
+++ fenliu/src/fenliu_server.erl
@@ -12,7 +12,7 @@
 
 %% API
 -export([start_link/4]).
--export([call/2, call/3, cast/2, nodes/1, add_nodes/2, delete_nodes/2]).
+-export([call/2, call/3, cast/2]).
  
 %% gen_server callbacks
 -export([init/1, handle_call/3, handle_cast/2, handle_info/2,
@@ -26,8 +26,8 @@
 %%% API
 %%%===================================================================
 
-start_link(ServerName, Nodes, MaxFails, CallTimeout) when is_atom(ServerName), is_list(Nodes) ->
-    gen_server:start_link({local, fenliu:server_name(ServerName)}, [ServerName, Nodes, MaxFails, CallTimeout], []).
+start_link(ServerName, Nodes, MaxFails, CallTimeout) when is_atom(ServerName), is_list(Nodes), is_integer(MaxFails), is_integer(CallTimeout) ->
+    gen_server:start_link({local, fenliu:server_name(ServerName)}, ?SERVER, [ServerName, Nodes, MaxFails, CallTimeout], []).
 
 call(ServerName, Request) ->
     gen_server:call(fenliu:server_name(ServerName), {call, Request}).
@@ -38,14 +38,6 @@ call(ServerName, Request, Timeout) when is_integer(Timeout) ->
 cast(ServerName, Msg) ->
     gen_server:cast(fenliu:server_name(ServerName), {cast, Msg}).
 
-nodes(ServerName) ->
-    gen_server:call(fenliu:server_name(ServerName), nodes).
-
-add_nodes(ServerName, Nodes) when is_list(Nodes) ->
-    gen_server:cast(fenliu:server_name(ServerName), {add_nodes, Nodes}).
-
-delete_nodes(ServerName, Nodes) when is_list(Nodes) ->
-    gen_server:cast(fenliu:server_name(ServerName), {delete_nodes, Nodes}).
 
 %%%===================================================================
 %%% gen_server callbacks
@@ -65,23 +57,14 @@ handle_call(nodes, _From, State) ->
     {reply, {ok, NodeList}, State}.
 
 handle_cast({cast, Msg}, State) ->
-    NodeList = State#state.node_list,
-    ServerName = State#state.server_name,
-    case pop_a_node(NodeList) of
-	{Node, NodeList2} ->
-	    gen_server:cast({ServerName, Node}, Msg),
-	    NodeList3 = NodeList2 ++ [Node],
-	    {noreply, State#state{node_list = NodeList3}};
-	empty ->
-	    {norpely, State#state{node_list = []}}
-    end;
+    cast_node(Msg, State);
 handle_cast({add_nodes, Nodes}, State) ->
     NodeList = State#state.node_list,
     NewNodeList = [{N, State#state.max_fails} || N <- Nodes],
     {noreply, State#state{node_list = NodeList ++ NewNodeList}}; 
-handle_cast({delete_nodes, Nodes}, State) ->
+handle_cast({remove_nodes, Nodes}, State) ->
     NodeList = State#state.node_list,
-    NewNodeList = lists:dropwhile(fun({N, _}) -> lists:member(N, Nodes) end, NodeList),
+    NewNodeList = lists:filter(fun({N, _}) -> not lists:member(N, Nodes) end, NodeList),
     {noreply, State#state{node_list = NewNodeList}}.
 
 handle_info(_Info, State) ->
@@ -101,24 +84,42 @@ call_node(Request, State) ->
     NodeList = State#state.node_list,
     ServerName = State#state.server_name,
     CallTimeout = State#state.call_timeout,
-    case pop_a_node(NodeList) of
-	{Node, NodeList2} ->
-	    case catch gen_server:call({ServerName, Node}, Request, CallTimeout) of
-		{'EXIT', {timeout, _}} ->
-		    NodeList3 = NodeList2 ++ dec_fail_count(Node),
-		    call_node(Request, State#state{node_list = NodeList3});
-		Reply ->
-		    NodeList3 = NodeList2 ++ [Node],
-		    {reply, Reply, State#state{node_list = NodeList3}}
-	    end;
-	empty ->
-	    {reply, {error, no_node}, State#state{node_list = []}}
+    NodeList = State#state.node_list,
+    case call_node_1(ServerName, Request, CallTimeout, NodeList) of
+	{ok, Reply, NewNodeList} ->
+	    {reply, Reply, State#state{node_list = NewNodeList}};
+	{error, run_out_of_nodes} ->
+	    {reply, {error, run_out_of_nodes}, State#state{node_list = []}}
     end.
 
-pop_a_node([]) -> empty;
-pop_a_node([{_, 0}|T]) -> pop_a_node(T);
-pop_a_node([H|T]) -> {H, T}.
+call_node_1(_, _, _, []) -> {error, run_out_of_nodes};
+call_node_1(ServerName, Request, CallTimeout, [{_, 0}|T]) -> call_node_1(ServerName, Request, CallTimeout, T); 
+call_node_1(ServerName, Request, CallTimeout, [{Node, FailCount}|T]) -> 
+     case catch gen_server:call({ServerName, Node}, Request, CallTimeout) of
+	{'EXIT', {timeout, Trace}} ->
+	    {ok, {'EXIT', {timeout, Trace}}, T ++ [{Node, FailCount - 1}]};
+%	{'EXIT', {{nodedown, _}, _}} ->
+%	    call_node_1(Request, ServerName, CallTimeout, T ++ [{Node, FailCount - 1}]);
+%	{'EXIT', {noproc, _}} ->
+%	    call_node_1(Request, ServerName, CallTimeout, T ++ [{Node, FailCount - 1}]);
+	{'EXIT', Reason} ->
+	    error_logger:warning_msg(io_lib:format("call to {~p, ~p} failed, reaons: ~p~n", [ServerName, Node, Reason])),
+	    call_node_1(ServerName, Request, CallTimeout, T ++ [{Node, FailCount - 1}]);
+	Reply ->
+	    {ok, Reply, T ++ [{Node, FailCount}]}
+    end.
 
-dec_fail_count({_, 0}) -> [];
-dec_fail_count({N, FailCount}) -> [{N, FailCount - 1}].
+cast_node(Msg, State) ->
+    NodeList = State#state.node_list,
+    ServerName = State#state.server_name,
+    {ok, NewNodeList} = cast_node_1(ServerName, Msg, NodeList),
+    {noreply, State#state{node_list = NewNodeList}}.
+
+cast_node_1(_, _, []) -> {ok, []};
+cast_node_1(ServerName, Msg, [{_, 0}|T]) -> cast_node_1(ServerName, Msg, T);
+cast_node_1(ServerName, Msg, [{Node, FailCount}|T]) ->
+    gen_server:cast({ServerName, Node}, Msg),
+    {ok, T ++ [{Node, FailCount}]}.
+
+
 
