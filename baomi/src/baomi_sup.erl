79c660859d56fb17affbfec8e4640cc84f639ab6
unknown <yonghuan@3CNL09495.NOE.Nokia.com>
2012-05-18 23:33:11 +0800

    add fenliu logic in supervisor, spawn or not spawn child on local host

--- baomi/src/baomi_sup.erl
+++ baomi/src/baomi_sup.erl
@@ -21,7 +21,21 @@ start_link() ->
 %% ===================================================================
 
 init([]) ->
-    {ok, { {one_for_one, 5, 10}, [child_spec()]} }.
+    Env = application:get_all_env(),
+    case proplists:gen_value(fenliu, Env, false) of
+	{true, Nodes} when is_list(Nodes) ->
+	    case lists:member(local, Nodes) of
+		true ->
+		    Nodes2 = lists:map(fun(local) -> node(); (N) -> N end, Nodes),
+		    {ok, _} = fenliu:add_server(baomi_server, Nodes2),
+		    {ok, {{one_for_one, 5, 10}, [child_spec()]}};
+		false ->
+		    {ok, _} = fenliu:add_server(baomi_server, Nodes),
+		    {ok, {{one_for_one, 5, 10}, []}}
+	    end;
+	false ->
+	    {ok, {{one_for_one, 5, 10}, [child_spec()]}}
+    end.
 
 child_spec() ->
     {baomi_server,
