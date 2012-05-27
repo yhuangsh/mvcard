c7733c6bd126e9441287bce4ee1cff222b675eed
unknown <yonghuan@3CNL09495.NOE.Nokia.com>
2012-05-28 00:05:10 +0800

    remove

--- include/defs.hrl
+++
@@ -1,7 +0,0 @@
--define(SECONDS, (1)).
--define(MINUTES, (60)).
--define(HOURS, (60*?MINUTES)).
--define(DAYS, (24*?HOURS)).
-
--define(THOUSANDS, (1000)).
--define(MILLION, (1*?THOUSANDS*1*?THOUSANDS)).
