b10bd79b422fa21c47130c76ad723eb7ee47fa53
unknown <yonghuan@3CNL09495.NOE.Nokia.com>
2012-06-10 11:36:22 +0800

    nihao overhaul related changes

--- common/src/common.erl
+++ common/src/common.erl
@@ -1,8 +1,20 @@
 -module(common).
 
--export([now_in_seconds/0]).
-
+-export([now_in_seconds/0,
+	date_to_seconds/1,
+	datetime_to_seconds/1,
+	timestamp_to_seconds/1]).
 
 now_in_seconds() ->
-    Now = calendar:now_to_datetime(now()),
-    calendar:datetime_to_gregorian_seconds(Now).
+    timestamp_to_seconds(now()).
+
+date_to_seconds({Y, M, D}) ->
+    datetime_to_seconds({{Y, M, D}, {0, 0, 0}}).
+
+datetime_to_seconds({{Y, M, D}, {H, Min, S}}) ->
+    Dt = {{Y, M, D}, {H, Min, S}},
+    calendar:datetime_to_gregorian_seconds(Dt).
+
+timestamp_to_seconds({X, Y, Z}) ->
+    Dt = calendar:now_to_datetime({X, Y, Z}),
+    calendar:datetime_to_gregorian_seconds(Dt).
--- fenliu/include/fenliu.hrl
+++ fenliu/include/fenliu.hrl
@@ -1,7 +1,15 @@
 -ifdef(TEST).
+-ifdef(USE_FENLIU_CALL).
 fl_call(Server, Request) -> gen_server:call(Server, Request).
+-endif.
+-ifdef(USE_FENLIU_CAST).
 fl_cast(Server, Msg) -> gen_server:cast(Server, Msg).
+-endif.
 -else.
+-ifdef(USE_FENLIU_CALL).
 fl_call(Server, Request) -> (fenliu:resolve_server(Server)):call(Server, Request).
+-endif.
+-ifdef(USE_FENLIU_CAST).
 fl_cast(Server, Msg) -> (fenliu:resolve_server(Server)):cast(Server, Msg).
+-endif.
 -endif.
