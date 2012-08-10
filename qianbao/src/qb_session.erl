a7efa2ae9b5483965c1315b59d65b4169c0ff81d
yhuangsh <yong.huang.sh@gmail.com>
2012-08-11 01:29:19 +0800

    fix username/password verification bug

--- qianbao/src/qb_session.erl
+++ qianbao/src/qb_session.erl
@@ -12,8 +12,9 @@ update(Session) -> qb_tab_session:update(Session).
 
 login(UserId, Password) -> login_1(qb_user:get(UserId), Password).
 login_1(#qbu{id=UserId, pwd=Password}, Password) -> create(UserId);
-login_1(#qbu{pwd=_DifferentPassword}, _Password) -> login_2(undefined).
-login_2(undefined) -> {nok, "用户名或密码错"}.
+login_1(#qbu{id=UserId, pwd=_DifferentPassword}, _Password) -> login_error();
+login_1(undefined, _) -> login_error().
+login_error() -> {nok, "用户名或密码错"}.
 
 create(UserId) -> create(session_id(UserId), UserId).
 create(SessionId, UserId) -> create_1(qb_tab_session:read(SessionId), SessionId, UserId).
