9e2014fdc127db834530b4fcef1aefa9fcdc47c6
yhuangsh <yong.huang.sh@gmail.com>
2012-08-11 01:31:03 +0800

    rewrite url encode stuffs

--- qianbao/src/qb_url.erl
+++ qianbao/src/qb_url.erl
@@ -3,41 +3,36 @@
 
 -include("../../deps/yaws/include/yaws_api.hrl").
 
-this(A) -> lists:flatten(yaws_api:format_url(yaws_api:request_url(A))).
+e(Url) -> yaws_api:url_encode(Url).
+f(Url) -> lists:flatten(Url).
+
+this(A) -> this_1((A#arg.req)#http_request.path).
+this_1({abs_path, Path}) -> Path.
+	    
+index() -> f([prefix(), "index.yaws"]).
+index(Url) -> f([prefix(), "index.yaws?g=", e(Url)]).
     
-index() -> prefix() ++ "index.yaws".
-index(Url) -> prefix() ++ "index.yaws?g=" ++ yaws_api:url_encode(Url).
-    
-home() -> prefix() ++ "home.yaws".
-home(City) ->prefix() ++ "home.yaws?city=" ++ yaws_api:url_encode(City).
+home() -> f([prefix(), "home.yaws"]).
+home(City) -> f([prefix(), "home.yaws?city=", e(City)]).
 
 login() -> login(home()).
-login(CallbackUrl) -> prefix() ++ "login.yaws?cb=" ++ yaws_api:url_encode(CallbackUrl).
-account(UserId) -> prefix() ++ "account.yaws?uid=" ++ yaws_api:url_encode(UserId).
+login(CallbackUrl) -> f([prefix(), "login.yaws?cb=", e(CallbackUrl)]).
 
-select_city(City) -> prefix() ++ "selcity.yaws?city=" ++ yaws_api:url_encode(City).
-change_city(City) -> prefix() ++ "chgcity.yaws?city=" ++ yaws_api:url_encode(City).
+account(UserId) -> f([prefix(), "account.yaws?uid=", e(UserId)]).
 
-auth(UserName, Password, CallbackUrl) ->
-    prefix() ++ 
-	"auth.yaws?u=" ++ yaws_api:url_encode(UserName) ++ 
-	"&p=" ++ yaws_api:url_encode(Password) ++ 
-	"&cb=" ++ yaws_api:url_encode(CallbackUrl).
-register(CallbackUrl) -> prefix() ++ "register.yaws?cb=" ++ yaws_api:url_encode(CallbackUrl).
-barcode_scan(CallbackUrl) -> prefix_qb() ++ "barcode/scan?cb=" ++ yaws_api:url_encode(CallbackUrl).
+select_city(City) -> f([prefix(), "selcity.yaws?city=", e(City)]).
+
+change_city(City) -> f([prefix(), "chgcity.yaws?city=", e(City)]).
+
+auth(UserName, Password, CallbackUrl) -> f([prefix(), "auth.yaws?u=", e(UserName), "&p=", e(Password), "&cb=", e(CallbackUrl)]).
+
+register(CallbackUrl) -> f([prefix(), "register.yaws?cb=", e(CallbackUrl)]).
 
 error(Msg, Cb) -> error(Msg, "返回", Cb).
 error(Msg, BtnTxt, Cb) -> error("出错了", "原因", Msg, BtnTxt, Cb).
-error(Title1, Title2, Msg, BtnTxt, Cb) ->
-    lists:flatten([prefix(), 
-		   "error.yaws?t1=", yaws_api:url_encode(Title1), 
-		   "&t2=", yaws_api:url_encode(Title2), 
-		   "&m=", yaws_api:url_encode(Msg),
-		   "&b=", yaws_api:url_encode(BtnTxt),
-		   "&cb=", yaws_api:url_encode(Cb)]).
+error(Title1, Title2, Msg, BtnTxt, Cb) -> f([prefix(), "error.yaws?t1=", e(Title1), "&t2=", e(Title2), "&m=", e(Msg), "&b=", e(BtnTxt), "&cb=", e(Cb)]).
 
 %prefix() -> "http://admin.weilink.me:8000/qianbao/".
 %prefix() -> "http://localhost:8080/".
 prefix() -> "/".
-prefix_qb() -> "qianbao://".
    
